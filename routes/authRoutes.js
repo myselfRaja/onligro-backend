e24cba8900375bab2edadf0fbc5909c907f72690
Mohammed Raza <mohammedraza11239@gmail.com>
2025-11-30 17:29:15 +0530

    fix auth route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -76,10 +76,11 @@ router.post("/login", async (req, res) => {
     // Set token in cookie
 res.cookie("token", token, {
   httpOnly: true,
-  secure: false, // local me false rahega
-  sameSite: "lax",
-  maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  secure: true,     // render always https
+  sameSite: "none", // cross-site cookie allowed
+  maxAge: 7 * 24 * 60 * 60 * 1000,
 });
+
 
 
   const safeOwner = {
