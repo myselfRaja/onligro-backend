e4c1b3917742ce8ad8147fd99b1029f593013c4e
myselfRaja <rajaprofessional181261@gmail.com>
2025-11-30 15:49:43 +0530

    Update authRoutes.js
    lax to none

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -77,7 +77,7 @@ router.post("/login", async (req, res) => {
 res.cookie("token", token, {
   httpOnly: true,
   secure: false, // local me false rahega
-  sameSite: "lax",
+  sameSite: "none",
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 });
 
